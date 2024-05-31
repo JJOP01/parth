ed03682a6ac217ad3cab4f4d7dc07747eafc827e
JJOP01 <jjo.pumipi@gmail.com>
2024-05-31 23:07:06 +1200

    DONE: compile functions in assembly, removed uncessary code

--- CONTRIBUTING.md
+++
@@ -1,2 +0,0 @@
-
-No contributions are accepted right now. This project will at times be unstable.
--- hello.asm
+++
@@ -1,9 +0,0 @@
-%define SYS_EXIT 60
-
-segment .text
-global _start
-_start:
-	mov rax, SYS_EXIT
-	mov rdi, 0	
-	syscall
-	ret
